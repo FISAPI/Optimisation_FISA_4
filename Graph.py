cb998d2563ba3a8c1ea7734d4a55e0804831e300
Elena <elenabeylat@gmail.com>
2024-03-27 10:56:31 +0100

    feat : write a network

--- Graph.py
+++ Graph.py
@@ -1,47 +1,82 @@
 import math
 
+
 class Graph:
     def __init__(self):
-        self.vertices = {}
+        self.aretes = {}
+        self.sommet = {}
+        self.n = 0
+        self.m = 0
+        self.reseau = [[0 for _ in range(self.m)] for _ in range(self.n)]
 
-    def add_vertex(self, vertex_id):
-        if vertex_id not in self.vertices:
-            self.vertices[vertex_id] = {}
+    def read_file(self, filename):
+        with open(filename, 'r') as file:
+            line = file.readline().strip().split(' ')
+            self.n, self.m = int(line[0]), int(line[1])
+            self.reseau = [[0 for _ in range(self.m)] for _ in range(self.n)]
+            for i in range(self.n):
+                line = file.readline().strip().split(' ')
+                for j in range(self.m):
+                    if line[j] == '1':
+                        self.add_reseau_point(i, j, 1)
+                    elif line[j] == '2':
+                        self.add_reseau_point(i, j, 2)
+                    elif line[j] == '3':
+                        self.add_reseau_point(i, j, 3)
+                    elif line[j] == "0":
+                        self.add_reseau_point(i, j, 0)
 
-    def add_edge(self, v1, v2, weight):
-        if v1 in self.vertices and v2 in self.vertices:
-            self.vertices[v1][v2] = weight
-            self.vertices[v2][v1] = weight
+    def add_reseau_point(self, i, j, nb):
+        try:
+            self.reseau[i][j] = nb
+        except Exception as e:
+            print(e)
 
-    def remove_vertex(self, vertex_id):
-        if vertex_id in self.vertices:
-            del self.vertices[vertex_id]
-            for v in self.vertices.values():
-                if vertex_id in v:
-                    del v[vertex_id]
-
-    def remove_edge(self, v1, v2):
-        if v1 in self.vertices and v2 in self.vertices:
-            if v1 in self.vertices[v2] and v2 in self.vertices[v1]:
-                del self.vertices[v1][v2]
-                del self.vertices[v2][v1]
-
-    def get_vertices(self):
-        return list(self.vertices.keys())
-
-    def get_neighbors(self, vertex_id):
-        if vertex_id in self.vertices:
-            return list(self.vertices[vertex_id].keys())
-        else:
-            return []
-
-    def get_edge_weight(self, v1, v2):
-        if v1 in self.vertices and v2 in self.vertices:
-            if v1 in self.vertices[v2] and v2 in self.vertices[v1]:
-                return self.vertices[v1][v2]
-            else:
-                return None
-        else:
-            return None
+    def display_reseau(self):
+        for i in range(self.n):
+            print(self.reseau[i])
+            print("\n")
 
 
+    # def add_reseau(self, reseau):
+    #     self.reseau = reseau
+
+    # def add_arete(self, arete_id):
+    #     if arete_id not in self.aretes:
+    #         self.aretes[arete_id] = {}
+    #
+    # def add_edge(self, v1, v2, weight):
+    #     if v1 in self.aretes and v2 in self.aretes:
+    #         self.aretes[v1][v2] = weight
+    #         self.aretes[v2][v1] = weight
+    #
+    # def remove_arete(self, arete_id):
+    #     if arete_id in self.aretes:
+    #         del self.aretes[arete_id]
+    #         for v in self.aretes.values():
+    #             if arete_id in v:
+    #                 del v[arete_id]
+    #
+    # def remove_edge(self, v1, v2):
+    #     if v1 in self.aretes and v2 in self.aretes:
+    #         if v1 in self.aretes[v2] and v2 in self.aretes[v1]:
+    #             del self.aretes[v1][v2]
+    #             del self.aretes[v2][v1]
+    #
+    # def get_aretes(self):
+    #     return list(self.aretes.keys())
+    #
+    # def get_neighbors(self, arete_id):
+    #     if arete_id in self.aretes:
+    #         return list(self.aretes[arete_id].keys())
+    #     else:
+    #         return []
+    #
+    # def get_edge_weight(self, v1, v2):
+    #     if v1 in self.aretes and v2 in self.aretes:
+    #         if v1 in self.aretes[v2] and v2 in self.aretes[v1]:
+    #             return self.aretes[v1][v2]
+    #         else:
+    #             return None
+    #     else:
+    #         return None
--- utils.py
+++ utils.py
@@ -1,5 +1,5 @@
 import math
-import Graph
+from Graph import Graph
 
 
 def create_graph_from_file(filename):
@@ -11,30 +11,36 @@ def create_graph_from_file(filename):
             line = file.readline().strip().split(' ')
             for j in range(m):
                 if line[j] == '1':
-                    graph.add_vertex((i, j))
+                    graph.add_arete((i, j))
                 elif line[j] == '2':
-                    graph.add_vertex((i, j))
+                    graph.add_arete((i, j))
                     start = (i, j)
                 elif line[j] == '3':
-                    graph.add_vertex((i, j))
+                    graph.add_arete((i, j))
                     end = (i, j)
         for i in range(n):
             for j in range(m):
-                if (i, j) in graph.vertices:
-                    if i > 0 and (i-1, j) in graph.vertices:
+                if (i, j) in graph.aretes:
+                    if i > 0 and (i-1, j) in graph.aretes:
                         graph.add_edge((i, j), (i-1, j), 1)
-                    if i < n-1 and (i+1, j) in graph.vertices:
+                    if i < n-1 and (i+1, j) in graph.aretes:
                         graph.add_edge((i, j), (i+1, j), 1)
-                    if j > 0 and (i, j-1) in graph.vertices:
+                    if j > 0 and (i, j-1) in graph.aretes:
                         graph.add_edge((i, j), (i, j-1), 1)
-                    if j < m-1 and (i, j+1) in graph.vertices:
+                    if j < m-1 and (i, j+1) in graph.aretes:
                         graph.add_edge((i, j), (i, j+1), 1)
-                    if i > 0 and j > 0 and (i-1, j-1) in graph.vertices:
+                    if i > 0 and j > 0 and (i-1, j-1) in graph.aretes:
                         graph.add_edge((i, j), (i-1, j-1), math.sqrt(2))
-                    if i > 0 and j < m-1 and (i-1, j+1) in graph.vertices:
+                    if i > 0 and j < m-1 and (i-1, j+1) in graph.aretes:
                         graph.add_edge((i, j), (i-1, j+1), math.sqrt(2))
-                    if i < n-1 and j > 0 and (i+1, j-1) in graph.vertices:
+                    if i < n-1 and j > 0 and (i+1, j-1) in graph.aretes:
                         graph.add_edge((i, j), (i+1, j-1), math.sqrt(2))
-                    if i < n-1 and j < m-1 and (i+1, j+1) in graph.vertices:
+                    if i < n-1 and j < m-1 and (i+1, j+1) in graph.aretes:
                         graph.add_edge((i, j), (i+1, j+1), math.sqrt(2))
     return graph, start, end
+
+
+if __name__ == '__main__':
+    graph = Graph()
+    graph.read_file(filename='exos/reseau_5_10_1.txt')
+    graph.display_reseau()
